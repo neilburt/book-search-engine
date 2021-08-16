24bef982e09093b933a876893d7de863e7ea06f8
neilburt <neil.burt@comcast.net>
2021-08-16 17:41:35 -0500

    Rebuilt server.js.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+    console.log(`${PORT} is GO!`)
+    console.log(`http://localhost:${PORT}${server.graphqlPath}`)
+  })
+})
